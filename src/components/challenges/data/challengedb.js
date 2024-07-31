81e76e22ecbf61fdeae8d42f6c5ea66ecddaf872
MalwareCube <malwarecube@gmail.com>
2024-07-30 22:57:49 -0400

    Added challenge 2

--- src/components/challenges/data/challengedb.js
+++ src/components/challenges/data/challengedb.js
@@ -3,6 +3,10 @@ const challengedb = [
       id: "074e4448-e8d7-4122-86f2-36a4d7b2a18b",
       data: require('./074e4448-e8d7-4122-86f2-36a4d7b2a18b.json'),
     },
+    {
+      id: "5e3f6ff6-46f7-4042-a969-34fd16451328",
+      data: require('./5e3f6ff6-46f7-4042-a969-34fd16451328.json'),
+    },
   ];
   
   export default challengedb;
