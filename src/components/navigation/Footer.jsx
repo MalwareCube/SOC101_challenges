0f6a203490b351d4059ddae8cebb5b715d4b70d1
MalwareCube <malwarecube@gmail.com>
2024-08-01 11:52:01 -0400

    Added hints

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <div className="container">
-                <p className="footer_info">This website is part of the <b>SOC 101</b> training course at <a href="https://academy.tcm-sec.com/" rel="noreferrer" target="_blank">TCM Academy</a>.</p>
+                <p className="footer_info">This challenge is part of the <b>SOC 101</b> training course at <a href="https://academy.tcm-sec.com/" rel="noreferrer" target="_blank">TCM Academy</a>.</p>
                 <p className="footer_copyright">&copy; <a href="https://tcm-sec.com/" rel="noreferrer" target="_blank">TCM Security</a>, Inc. {currentYear}</p>
             </div>
         </div>
